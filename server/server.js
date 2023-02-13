7c71d28037ec85ad22f1b4a7166a93653b06b20c
daisy <gid5214@naver.com>
2023-02-13 23:49:12 +0900

    feat: DB connect(sequelize)

--- server/server.js
+++ server/server.js
@@ -24,21 +24,21 @@ app.use(express.static(path.join(__dirname, 'public'))); // 요청시 기본 경
 app.use(express.json()); // json 파싱, 유저가 보낸 데이터 출력하기 위해 필요
 app.use(express.urlencoded({ extended: true })); // uri 파싱
 
-// // DB 연결 성공 여부
-// const { sequelize } = require('./model/index');
-// // 다른 require문은 일단 생략
-// const ConnectDB = async () => {
-//   try {
-//     await sequelize
-//       .authenticate()
-//       .then(() => console.log('데이터베이스 연결 성공!'));
-//     await sequelize.sync().then(() => console.log('동기화 완료!'));
-//   } catch (error) {
-//     console.error('DB 연결 및 동기화 실패', error);
-//   }
-// };
-// // DB와 연결 및 동기화
-// ConnectDB();
+// DB 연결 성공 여부
+const { sequelize } = require('./model/index');
+// 다른 require문은 일단 생략
+const ConnectDB = async () => {
+  try {
+    await sequelize
+      .authenticate()
+      .then(() => console.log('데이터베이스 연결 성공!'));
+    await sequelize.sync().then(() => console.log('동기화 완료!'));
+  } catch (error) {
+    console.error('DB 연결 및 동기화 실패', error);
+  }
+};
+// DB와 연결 및 동기화
+ConnectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`server port ${process.env.PORT} open`);
