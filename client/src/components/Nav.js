dd8ba450e2c713126a99108c0abc39db14437c0f
hatbann <0urloveis_great@naver.com>
2023-02-13 23:32:31 +0900

    feat:스위치 구현

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,22 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from './css/Nav.module.css';
 
+import SwitchBtn from './SwitchBtn';
+
 import logo from '../assets/Logo.png';
+import DogIcon from '../assets/DogIcon.png';
+import CatIcon from '../assets/CatIcon.png';
+import BasicIcon from '../assets/BasicIcon.png';
+
+import { useSelector } from 'react-redux';
 
 const Nav = () => {
-  const isLoggedIn = useState(false); //로그인 유무 변수
+  const isLoggedIn = useSelector((state) => state.user.user.isLoggedIn);
+  // const puppyBtnRef = useRef();
+  // const catBtnRef = useRef();
+  // const basicBtnRef = useRef();
 
+  const [swtichType, setSwitchType] = useState('');
+  const btnState = useSelector((state) => state.typeSwitch.switchState);
+
+  useEffect(() => {
+    if (btnState === 'basic') {
+      setSwitchType('기본');
+    } else if (btnState === 'puppy') {
+      setSwitchType('강아지');
+    } else {
+      setSwitchType('고양이');
+    }
+  }, [btnState]);
+
+  console.log(btnState);
   return (
     <div className={styles.nav}>
       <div>
         <NavLink to="/">
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" className={styles.logoIcon} />
         </NavLink>
+        {btnState === 'basic' ? (
+          <img src={BasicIcon} alt="basicIcon" className={styles.typeIcon} />
+        ) : (
+          ''
+        )}
+        {btnState === 'puppy' ? (
+          <img src={DogIcon} alt="dogIcon" className={styles.typeIcon} />
+        ) : (
+          ''
+        )}
+        {btnState === 'cat' ? (
+          <img src={CatIcon} alt="catIcon" className={styles.typeIcon} />
+        ) : (
+          ''
+        )}
       </div>
       <div className={styles.btns}>
-        <button>강아지</button>
+        <SwitchBtn />
+
         <button>Login</button>
       </div>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import rootReducer from './store';
+import { legacy_createStore as createStore } from 'redux';
+
 import './reset.css';
 
+const reduxDevTool =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, reduxDevTool);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
