acc19f7aee1753498eb2ec9cb0bf3c3e539c1445
daisy <gid5214@naver.com>
2023-02-16 13:53:27 +0900

    feat: sequelize foreign key 설정

--- server/controller/user/login.js
+++ server/controller/user/login.js
@@ -1,16 +1,33 @@
 const { user } = require('../../model');
 const axios = require('axios');
+require('dotenv').config();
 
-const kakao = {
-  clientID: process.env.kakaoClientID,
-  redirectUri: process.env.kakaoRedirectUri,
-  clientSecret: process.env.kakaoClientSecret,
-};
+const CLIENT_ID = process.env.KAKAO_CLIENTID;
+const REDIRECT_URI = process.env.KAKAO_REDIRECTURI;
+const CLIENT_SECRET = process.env.KAKAO_CLIENTSECRET;
 
-exports.viewKakaoLogin = (req, res) => {
-  `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}`;
-  console.log(kakao.clientID);
-  console.log(kakao.redirectUri);
+exports.kakaoCode = async (req, res) => {
+  console.log('query', req.query.code);
+  console.log(CLIENT_ID);
+  console.log(REDIRECT_URI);
+  console.log(CLIENT_SECRET);
+  token = await axios({
+    method: 'POST',
+    url: 'https://kauth.kakao.com/oauth/token',
+    Headers: {
+      'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+    data: {
+      grant_type: 'authorization_code',
+      client_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      code: req.query.code,
+      client_secret: CLIENT_SECRET,
+    },
+  }).then((res) => {
+    console.log(res.data);
+    res.redirect('http://localhost:3000');
+  });
 };
 
 exports.kakaoLogin = async (req, res) => {
