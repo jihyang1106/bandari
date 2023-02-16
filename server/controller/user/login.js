6cc0a985bcaef92ccc71aac33047275f4aac2c73
namhyong <namhyong2@gmail.com>
2023-02-16 13:57:17 +0900

    feat:kakao success

--- server/controller/user/login.js
+++ server/controller/user/login.js
@@ -1,16 +1,42 @@
-const { user } = require('../../model');
+const { user } = require('../../model/user');
 const axios = require('axios');
+require('dotenv').config();
 
-const kakao = {
-  clientID: process.env.kakaoClientID,
-  redirectUri: process.env.kakaoRedirectUri,
-  clientSecret: process.env.kakaoClientSecret,
-};
+const CLIENT_ID = process.env.KAKAO_CLIENTID;
+const REDIRECT_URI = process.env.KAKAO_REDIRECTURI;
+const CLIENT_SECRET = process.env.KAKAO_CLIENTSECRET;
 
-exports.viewKakaoLogin = (req, res) => {
-  `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}`;
-  console.log(kakao.clientID);
-  console.log(kakao.redirectUri);
+exports.kakaoCode = async (req, res) => {
+  // 로그인 후 받은 인가 코드로 토큰을 받기위한 요청을 카카오 서버로 보냄
+  token = await axios({
+    method: 'POST',
+    url: 'https://kauth.kakao.com/oauth/token',
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+    data: {
+      grant_type: 'authorization_code',
+      client_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      code: req.query.code,
+      client_secret: CLIENT_SECRET,
+    },
+  }).then((res) => {
+    // 토큰을 받아옴
+    const ACESS_TOKEN = res.data.access_token
+    const APP_ADMIN_KEY = 
+    console.log(res.data);
+    await axios.get({
+      method:'get',
+      url:'http://kapi.kakao.com/v2/user/me',
+      headers:{
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+      },
+      data:{
+        `Bearer ${ACCESS_TOKEN}/KakaoAK ${APP_ADMIN_KEY}`
+      }
+    })
+  });
 };
 
 exports.kakaoLogin = async (req, res) => {
