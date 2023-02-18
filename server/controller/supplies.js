9d56c55eaa6bbc1e7e5fc9821799493e9142f3ee
AHRUMPARK <arum4523@naver.com>
2023-02-18 15:23:33 +0900

    펫 프로필 파일 중간 수정

--- server/controller/supplies.js
+++ server/controller/supplies.js
@@ -7,14 +7,12 @@ const fs = require('fs');
 const axios = require('axios');
 const { Op } = require('sequelize');
 const user = require('../model/user');
-
 try {
   fs.readdirSync('../client/public/uploads');
 } catch (err) {
   console.error('upload 폴더가 없습니다. 폴더를 생성합니다.');
   fs.mkdirSync('../client/public/uploads');
 }
-
 const imgList = {};
 const upload = multer({
   storage: multer.diskStorage({
@@ -28,20 +26,22 @@ const upload = multer({
     },
   }),
 });
-
 /** 업로드 하나만 */
 // router.post('/insert', upload.single('img'), async (req, res) => {
 //   console.log('req.file', req.file);
 //   console.log('req.body', JSON.parse(req.body.data));
-
 //   // const result = await User.create({});
 //   // console.log(result);
 //   res.send(true);
 // });
-
+/** 업로드 여러개 */
+(exports.postInsert = upload.array('img')),
+  async (req, res) => {
+    console.log(req.body);
+    console.log(req.files);
+  };
 
 // 좋아요 조회
 // exports.getLikeCount = async (req, res) => {
 //   await
 // }
-
