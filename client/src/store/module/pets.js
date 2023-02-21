1d35a248f92e3d1da99720f8fdf968ac2deea285
hatbann <0urloveis_great@naver.com>
2023-02-21 15:26:58 +0900

    feat:혜빈 수정

--- client/src/store/module/pets.js
+++ client/src/store/module/pets.js
@@ -5,14 +5,14 @@ const initialState = {
 function setPets(pet) {
   console.log('pet', pet);
   return {
-    type: 'SETPETS',
+    type: 'pets/SETPETS',
     pet,
   };
 }
 
 function pets(state = initialState, action) {
   switch (('pet 액션 타입 :', action.type)) {
-    case 'SETPETS':
+    case 'pets/SETPETS':
     // const indexpets = state.pets.push(action.pet);
     // return { ...state, pets: indexpets };
     // return {
