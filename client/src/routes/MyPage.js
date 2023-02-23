68e8e3a226e376b46e28aeb98e71665cd56c3bd9
hatbann <0urloveis_great@naver.com>
2023-02-23 13:29:45 +0900

    feat:마이페이지

--- client/src/routes/MyPage.js
+++ client/src/routes/MyPage.js
@@ -152,18 +152,23 @@ const MyPage = (props) => {
 
   /**로그인한 유저가 찜한 판매글 가져오는 함수*/
   const getLikeData = () => {
-    axios.get('pick/userPick',{
-      params: {
-      userId:isLoggedIn
-      }
-    }).then((res)=>{
-      // console.log("찜",res.data)
-      const userPick= [];
-      for(let i = 0; i<res.data.length; i++)
-      {userPick.push(res.data[i].supply)}
-      console.log("어레이",userPick)
-      // setLike(userPick)
-    })
+    axios
+      .get('pick/userPick', {
+        params: {
+          userId: isLoggedIn,
+        },
+      })
+      .then((res) => {
+        // console.log("찜",res.data)
+        const userPick = [];
+        for (let i = 0; i < res.data.length; i++) {
+          userPick.push(res.data[i].supply);
+        }
+        console.log('어레이', userPick);
+        // setLike((prev) => {
+        //   return [...prev, userPick];
+        // });
+      });
   };
 
   /**펫 추가 이벤트 */
